1488df75cb887e06c7f1f68ba769f10b201731ac
shawon39 <shshawon39@gmail.com>
2019-07-21 10:15:41 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -7,14 +7,9 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
-import Clarifai from 'clarifai';
 import './App.css';
 import './components/Logo/Logo.css';
 import 'tachyons';
-
-const app = new Clarifai.App({
-  apiKey: 'be337d6035d24b4aaf827177992a6fc2'
- });
 
 const particlesOptions = {
   particles: {
@@ -37,10 +32,27 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
 
+  loadUser = (data) => {
+    //console.log(data);
+    this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+    }})
+  }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -67,10 +79,31 @@ class App extends Component {
 
     this.setState({ imageUrl: this.state.input });
 
-    app.models.predict( 
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    fetch('https://secret-atoll-74178.herokuapp.com/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            input: this.state.input
+        })
+    })
+    .then(response => response.json())
+    .then(response => {
+      if(response) {
+        fetch('https://secret-atoll-74178.herokuapp.com/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                id: this.state.user.id
+            })
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, {entries: count}))
+        })
+        .catch(console.log);
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
     .catch( err => console.log(err))
   }
 
@@ -95,7 +128,7 @@ class App extends Component {
           route === 'home'? 
             <div>
               <Logo />
-              <Rank /> 
+              <Rank name={this.state.user.name} entries={this.state.user.entries}/> 
               <ImageLinkForm 
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit}
@@ -105,9 +138,9 @@ class App extends Component {
               :
             (
               route === 'signin'?
-              <Signin onRouteChange={this.onRouteChange}/> 
+              <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/> 
               :
-              <Register onRouteChange={this.onRouteChange} />
+              <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
             )
         }
       </div>
